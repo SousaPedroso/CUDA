75f319391cafb20a52c57e9e1cc95e049f471c8a
SousaPedroso <gsousa844@gmail.com>
2023-01-13 11:08:27 -0400

    Memory management on CPU and GPU

---
+++ accelerating_applications/01-double-elements.cu
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+void init(int *a, int N)
+{
+  int i;
+  for (i = 0; i < N; ++i)
+  {
+    a[i] = i;
+  }
+}
+
+__global__
+void doubleElements(int *a, int N)
+{
+  int i;
+  i = blockIdx.x * blockDim.x + threadIdx.x;
+  if (i < N)
+  {
+    a[i] *= 2;
+  }
+}
+
+bool checkElementsAreDoubled(int *a, int N)
+{
+  int i;
+  for (i = 0; i < N; ++i)
+  {
+    if (a[i] != i*2) return false;
+  }
+  return true;
+}
+
+int main()
+{
+  int N = 100;
+  int *a;
+
+  size_t size = N * sizeof(int);
+
+  cudaMallocManaged(&a, size);
+
+  init(a, N);
+
+  size_t threads_per_block = 10;
+  size_t number_of_blocks = 10;
+
+  doubleElements<<<number_of_blocks, threads_per_block>>>(a, N);
+  cudaDeviceSynchronize();
+
+  bool areDoubled = checkElementsAreDoubled(a, N);
+  printf("All elements were doubled? %s\n", areDoubled ? "TRUE" : "FALSE");
+
+  cudaFree(a);
+}
