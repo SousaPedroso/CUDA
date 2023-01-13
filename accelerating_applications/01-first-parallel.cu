8781dc2b0fb8d45b83ac0218276d1e0f2320e905
SousaPedroso <gsousa844@gmail.com>
2023-01-12 20:42:17 -0400

    Parallel execution on GPU

---
+++ accelerating_applications/01-first-parallel.cu
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+__global__ void firstParallel()
+{
+  printf("This should be running in parallel.\n");
+}
+
+int main()
+{
+
+  firstParallel<<<5,5>>>();
+
+
+  cudaDeviceSynchronize();
+
+}
