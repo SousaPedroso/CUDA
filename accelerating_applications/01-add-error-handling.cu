70dd47479a3e31be1665d852386454e69d929a5d
SousaPedroso <gsousa844@gmail.com>
2023-01-14 19:54:13 -0400

    Error Handling

---
+++ accelerating_applications/01-add-error-handling.cu
@@ -0,0 +1,71 @@
+#include <stdio.h>
+
+void init(int *a, int N)
+{
+  int i;
+  for (i = 0; i < N; ++i)
+  {
+    a[i] = i;
+  }
+}
+
+__global__
+void doubleElements(int *a, int N)
+{
+
+  int idx = blockIdx.x * blockDim.x + threadIdx.x;
+  int stride = gridDim.x * blockDim.x;
+
+  for (int i = idx; i < N + stride; i += stride)
+  {
+    a[i] *= 2;
+  }
+}
+
+bool checkElementsAreDoubled(int *a, int N)
+{
+  int i;
+  for (i = 0; i < N; ++i)
+  {
+    if (a[i] != i*2) return false;
+  }
+  return true;
+}
+
+int main()
+{
+
+  int N = 10000;
+  int *a;
+  cudaError_t err;
+
+  size_t size = N * sizeof(int);
+  
+  err = cudaMallocManaged(&a, size);
+  
+  if (err != cudaSuccess){
+      printf("Error: %s\n", cudaGetErrorString(err));
+      exit(0);
+  }
+
+  init(a, N);
+
+  size_t threads_per_block = 2048;
+  size_t number_of_blocks = 32;
+
+  doubleElements<<<number_of_blocks, threads_per_block>>>(a, N);
+  // Definition of kernel error handling
+  err = cudaGetLastError();
+  
+  if (err != cudaSuccess){
+      printf("Error: %s\n", cudaGetErrorString(err));
+      exit(0);
+  }
+  
+  cudaDeviceSynchronize();
+    
+  bool areDoubled = checkElementsAreDoubled(a, N);
+  printf("All elements were doubled? %s\n", areDoubled ? "TRUE" : "FALSE");
+
+  cudaFree(a);
+}
