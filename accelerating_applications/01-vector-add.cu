6cfae67fa6b6d0c2f0950a1dc142f2a51b7f609e
SousaPedroso <gsousa844@gmail.com>
2023-01-14 20:21:25 -0400

    Final exercise of the module

---
+++ accelerating_applications/01-vector-add.cu
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <assert.h>
+
+__global__ void initWith(float num, float *a, int N)
+{
+  int threadId, gridStride;
+  threadId = threadIdx.x + blockIdx.x * blockDim.x;
+  gridStride = blockDim.x * gridDim.x;
+
+  for(int i = threadId; i < N; i+= gridStride){
+    if (i < N) a[i] = num;
+  }
+}
+
+__global__ void addVectorsInto(float *result, float *a, float *b, int N)
+{
+  int threadId, gridStride;
+  threadId = threadIdx.x + blockIdx.x * blockDim.x;
+  gridStride = blockDim.x * gridDim.x;
+
+  for(int i = threadId; i < N; i+=gridStride){
+    if (i<N) result[i] = a[i] + b[i];
+  }
+}
+
+void checkElementsAre(float target, float *array, int N)
+{
+
+  for(int i = 0; i < N; i+=1){
+    if(array[i] != target){
+      printf("FAIL: array[%d] - %0.0f does not equal %0.0f\n", i, array[i], target);
+      exit(1);
+    }
+  }
+  printf("SUCCESS! All values added correctly.\n");
+}
+
+cudaError_t checkCuda(cudaError_t result){
+    if (result != cudaSuccess){
+        fprintf(stderr, "CUDA RUNTIME ERROR: %s\n", cudaGetErrorString(result));
+        assert(result == cudaSuccess);
+    }
+    return result;
+}
+
+int main()
+{
+  const int N = 2<<20, blocks=32, threadsPerBlock=256;
+  size_t size = N * sizeof(float);
+
+  float *a;
+  float *b;
+  float *c;
+
+  if (checkCuda(cudaMallocManaged(&a, size)) != cudaSuccess){
+    exit(1);
+  }
+
+  if (checkCuda(cudaMallocManaged(&b, size)) != cudaSuccess){
+    exit(1);
+  }
+
+  if (checkCuda(cudaMallocManaged(&c, size)) != cudaSuccess){
+    exit(1);
+  }
+
+  initWith<<<blocks, threadsPerBlock>>>(3, a, N);
+  initWith<<<blocks, threadsPerBlock>>>(4, b, N);
+  initWith<<<blocks, threadsPerBlock>>>(0, c, N);
+
+  addVectorsInto<<<blocks, threadsPerBlock>>>(c, a, b, N);
+  if (checkCuda(cudaDeviceSynchronize()) != cudaSuccess){
+    exit(1);
+  }
+
+  checkElementsAre(7, c, N);
+
+  if (checkCuda(cudaFree(a)) != cudaSuccess){
+    exit(1);
+  }
+
+  if (checkCuda(cudaFree(b)) != cudaSuccess){
+    exit(1);
+  }
+
+  if (checkCuda(cudaFree(c)) != cudaSuccess){
+    exit(1);
+  }
+}
